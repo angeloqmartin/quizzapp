1fd30b8c61473f9a04c21a11f0af8b49ad394978
Angelo Martin <angeloqmartin@gmail.com>
2018-10-29 11:33:06 -0700

    init commit

---
+++ database.js
@@ -0,0 +1,114 @@
+const STORE = [
+    {
+        question: 'In what year was Nintendo founded?',
+        answer: [
+            '1889',
+            '1701',
+            '2000',
+            '1983',
+        ],
+        correctAnswer:'1889',
+        explanation:"Nintendo was founded on September 23, 1889 by Fusajiro Yamauchi in Kyoto, Japan, as an Playing Card Company.",
+    },
+    {
+        question: 'What video game has sold the most copies to date?',
+        answer: [
+            'Super Mario Bros.',
+            'Call of Duty: Black Ops',
+            'Grand Theft Auto V',
+            'Tetris',
+
+        ],
+        correctAnswer:'Tetris',
+        explanation: 'With an astounding 170 million copies sold, Tetris is by far the most sold game ever. The fact it was first put on sale over three decades ago helps',
+    },
+    {
+        question:'What is the maximum score that can be reached in Pac-Man',
+        answer: [
+            '1,000,000',
+            '5,000',
+            '3,333,360',
+            '9,640,100',
+        ],
+        correctAnswer: '3,333,360',
+        explanation: 'The highest score you can get in Pac-Man is 3,333,360. This is achived doing the first 255 levels by eating every possible dot, power pellet, fruit, and enemy, without losing a single life, and using all extra lives. The first person to achieve this score was Billy Mitchell in 1999.',
+    },
+    {
+        question:'What is the best-selling console of all time?',
+        answer: [
+            'PlayStation 2',
+            'Nintnedo: NES',
+            'Playstation 4',
+            'Xbox 360',
+        ],
+        correctAnswer: 'PlayStation 2',
+        explanation: 'With 156 million units sold Playstation 2 is the best selling unit to date.',
+    },
+    {
+        question:"From the famous Nintendo game , what is Mario's and Luigi's last name?",
+        answer: [
+            'Roberto',
+            'Mario',
+            'Pepperoni',
+            'Angelo',
+        ],
+        correctAnswer: 'Mario',
+        explanation: 'Their last name is Mario, Mario\'s full name is “Mario Mario”. As for Luigi, he\'s “Luigi Mario”. During Japan\'s 30th anniversary Super Mario Bros Shigeru Miyamoto cleared this up.',
+    },
+    {
+        question:'Who is the main playable character name in the game Legend of Zelda?',
+        answer: [
+            'Ganondorf',
+            'Zelda',
+            'Link',
+            'Sheik',
+        ],
+        correctAnswer: 'Link',
+        explanation: 'Link is the main protagonist of Nintendo\'s video game series The Legend of Zelda.',
+    },
+    {
+        question:'This famous cheat code “up, up, down, down, right, left, right, left, B, A, START” is known as what?',
+        answer: [
+            'The Konami Code',
+            'Cheat Code',
+            'The Contra code',
+            'Code of Honor',
+
+        ],
+        correctAnswer: 'The Konami Code',
+        explanation: 'This sequence of keystrokes is called "The Konami Code". It allows players to enable special features in video games made by the Japanese company Konami.',
+    },
+    {
+        question:'What was Microsoft\'s first console?',
+        answer: [
+            'Xbox',
+            'Dreamcast',
+            'Gamecube',
+            'Xbox 360',
+        ],
+        correctAnswer: 'Dreamcast',
+        explanation: 'Okay this can be argumenting, although is was a partnership Microsft first console was the Dreamcast. On May 21, 1998, Microsoft proudly announced its partnership with Sega on the Dreamcast, the "ultimate home video game system". What started out as a coming together of two giants, one of gaming, one of computing, ultimately fizzled out into being nothing more than a memory.',
+    },
+    {
+        question:'What was the longest time an individual has played a video game without any breaks?',
+        answer: [
+            '135 hours',
+            '8 hours',
+            '48 hours',
+            '96 hours',
+        ],
+        correctAnswer: '135 hours',
+        explanation: 'Determined by Okan Kaya which played Call of Duty: Black Ops 2 for a total of 135 hours. Thats more than five days!',
+    },
+    {
+        question:'In what year was the first game in the Halo series released?',
+        answer: [
+            '2001',
+            '2000',
+            '2005',
+            '1999',
+        ],
+        correctAnswer: '2001',
+        explanation: 'The first game of the Halo franchise, it was released as a launch title for the Xbox gaming system on November 15, 2001.',
+    },
+];
---
+++ script.js
@@ -0,0 +1,251 @@
+'use strict';
+
+// ---- VARIABLE DEFINITION ----
+let questionNumber = 0;
+let questionTracker = 1;
+let answeredCorrect = 0;
+let answeredIncorrect = 0;
+
+function resetVariableDefinition() {
+  questionNumber = 0;
+  questionTracker = 1;
+  answeredCorrect = 0;
+  answeredIncorrect = 0;
+}
+
+// REMOVES THE START PAGE AND
+// START PAGE CONTENT 
+function hideStartpage() {
+    $('.start-page').addClass('hide');
+};
+
+function showStartPage() {
+  $('.start-page').removeClass('hide');
+}
+
+// REMOVES HIDE CLASS FROM MAIN QUIZ CONTENT 
+// SO THAT QUIZ FORM CAN SHOW 
+function removeHideclass() {
+    $('.main-container').removeClass('hide');
+};
+
+// ADDS 'HIDE' CLASS TO MAIN QUIZ CONTENT
+// (TO THE .MAIN-CONTAINER CLASS)
+// SO THAT QUIZ FORM IS HIDDEN
+function addHideclass() {
+    $('.main-container').addClass('hide');
+};
+
+// WHEN START BUTTON IS PRESSED REMOVES START PAGE
+// REMOVE HIDE CLASS TO REVEAL QUIZ FORM
+function handleStartButton() {
+  $('.start-page').on('click', 'button', function(){
+    hideStartpage();
+    removeHideclass();
+  });
+};
+
+// RETURNS QUESTION AND SCORE TRACKER
+function trackerElements() {
+  return `
+  <p class="question-tracker">Question ${questionTracker} of 10</p> <p class="score-tracker">Score: ${answeredCorrect} of 10</p>`;
+};
+
+// RENDERS TRACKER ELEMENTS TO THE DOM
+function renderTracker() {
+$('.tracker').html(trackerElements());
+};
+
+// WILL INCREMENT QUESTION TRACKER BY 1
+function increaseQuestionTracker() {
+  questionTracker++;
+};
+
+// WILL INCREMENT SCORE TRACKER BY 1
+// IF USER ANSWERED CORRECTLY
+function increaseAnsweredCorrect() {
+  answeredCorrect++;
+};
+
+// WILL INCREMENT INCORRECT TRACKER BY 1
+// IF USER ANSWERED INCORRECTLY
+function increaseAnsweredIncorrect() {
+  answeredIncorrect++;
+};
+
+// WILL INCREMENT QUESTION TRACKER BY 1
+function increaseQuestionNumber() {
+questionNumber++;
+};
+
+// RETURNS THE QUIZ APP CURRENT QUESTION  
+function currentQuestion() {
+    return `${STORE[questionNumber].question}`;
+};
+
+// RENDERS THE QUIZ APP CURRENT QUESTION IN THE DOM
+function renderQuizAppQuestion() {
+    $('.current-qusetion').html(currentQuestion());
+};
+
+function possibleAnswers() {
+  let answerHTML = ``;
+  for(let i = 0; i < STORE[questionNumber].answer.length; ++i) {
+  answerHTML += `<input type="radio" role="radio "name="answer" id="answer${i}" value="${STORE[questionNumber].answer[i]}" required><label for="answer${i}">${STORE[questionNumber].answer[i]}</label><br><br>`;
+  }
+  return answerHTML;
+};
+
+//RENDER THE QUIZ APP POSSIBLE ANSWERS IN THE DOM
+function renderPossibleAnswers() {
+  $('.possible-answers').html(possibleAnswers());
+};
+
+// LISTEN FOR CLICKS ON THE SUBMIT BUTTON
+function submitQuestionHandler() {
+  $(".submit-button").on('click', function(event) {
+    event.preventDefault();
+    disableSubmitQuestion();
+    checkUserAnswer();
+  });
+};
+
+function disableSubmitQuestion() {
+  $('.submit-button').prop('disabled', true);
+};
+
+function enableSubmitQuestion() {
+  $('.submit-button').prop('disabled', false);
+};
+
+// FUNCTION CHECKS USER ANSWER
+// WILL LET USER KNOW IF ANSWER WAS RIGHT OR WRONG
+function checkUserAnswer() {
+  let selected = $('input:checked');
+  let userAnswer = selected.val();
+  let correctAnswer = `${STORE[questionNumber].correctAnswer}`;
+  if (userAnswer === undefined) {
+    window.alert("PLEASE SELECT AN ANSWER!");
+    enableSubmitQuestion();
+  } else if (userAnswer == correctAnswer) {
+    userAnswerCorrectFeedback();
+    increaseAnsweredCorrect();
+  } else {
+    userAnswerIncorrectFeedback();
+    increaseAnsweredIncorrect();
+  }
+};
+
+// RETURNS FEEDBACK FOR USER IF ANSWER WAS CORRECT
+function renderFeedbackPage(){
+  $('#feedbackPage').css("display", "block");
+};
+
+function userAnswerCorrectFeedback() {
+  renderFeedbackPage();
+  $('.userFeedback-content').html(`<p>Correct!<p>
+  ${STORE[questionNumber].explanation}<br><br>
+  <button role="button" class="nextQuestion">Next Question</button>`);
+};
+
+// RETURNS FEEDBACK FOR USER IF ANSWER WAS INCORRECT
+function userAnswerIncorrectFeedback() {
+  renderFeedbackPage();
+  $('.userFeedback-content').html(`<p>Sorry that was incorrect<p>
+  ${STORE[questionNumber].explanation}<br><br>
+  <button role="button" class="nextQuestion">Next Question</button>`);
+};
+
+// HANDLES THE NEXT BUTTON ON THE FEEDBACK PAGE
+function nextButtonHandler() {
+  $('.userFeedback-content').on('click','button', function() {
+    nextStep();
+    enableSubmitQuestion();
+  });
+}
+
+function nextStep() {
+  if (questionTracker == STORE.length) {
+    closeFeedbackPage();
+    quizResults();
+  } else {
+    closeFeedbackPage();
+    nextQuestion();
+  };
+};
+
+function closeFeedbackPage() {
+  $('#feedbackPage').css("display", "none");
+};
+ 
+// DEFINE WHAT NEEDS TO HAPPENS IN ORDER TO MOVE TO NEXT QUESTION
+ function nextQuestion() {
+  increaseQuestionTracker();
+  increaseQuestionNumber();
+  renderQuizAppQuestion();
+  renderPossibleAnswers();
+  renderTracker();
+};
+
+function quizResults() {
+    addHideclass();
+    renderQuizResults();
+};
+
+// PRINT FINAL SCORE AND OPTION TO RESTART QUIZ
+function endOfQuizResults() {
+  if (answeredCorrect > 6) {
+    return `
+    <section class="resultsContent">
+      <h3>YOUR SCORE: ${answeredCorrect} CORRECT / ${answeredIncorrect} INCORRECT</h3>
+      <p>If you would like to retake Quiz, 'click restart'</p>
+      <div role="img" class="resultImg"><img src="url.img/h8XDfXS.jpg" alt="image of vault boy from fallout giving a thumbs up"></div>
+      <button role="button" class="restart-quiz-button">Restart</button>    
+    </section>
+    `;
+  } else {
+    return `
+    <section class="resultsContent">
+      <h3>YOUR SCORE: ${answeredCorrect} CORRECT / ${answeredIncorrect} INCORRECT</h3>
+      <p>If you would like to retake Quiz, 'click restart'</p>
+      <div role="img" class="resultImg"><img src="url.img/1465601601445.png" alt="image of vault boy from fallout with hand on forehead"></div>
+      <button role="button" class="restart-quiz-button">Restart</button>
+    <section>
+    `;
+  };
+};
+
+// RENDER THE RESULTS PAGE AT THE END OF THE QUIZ
+function renderQuizResults() {
+    $('.resultsPage').html(endOfQuizResults());
+    restartButtonHandler();
+};
+
+// HANDLES THE RESTART BUTTON AT THE END OF THE QUIZ
+function restartButtonHandler(){
+  $('.resultsContent').on('click', 'button', function() {
+    removeRestartPage();
+    showStartPage();
+    resetVariableDefinition();
+    renderTracker();
+    renderQuizAppQuestion();
+    renderPossibleAnswers();
+  });
+};
+
+function removeRestartPage() {
+  $('.resultsContent').remove();
+}
+
+// THIS FUNCTION WILL BE OUR CALLBACK WHEN THE PAGE LOADS
+function handleQuizApp() {
+    handleStartButton();
+    renderQuizAppQuestion();
+    renderPossibleAnswers();
+    renderTracker();
+    submitQuestionHandler();
+    nextButtonHandler();
+  };
+  
+// WHEN THE PAGE LOADS, CALL `HANDLEQUIZAPP`
+$(handleQuizApp)
